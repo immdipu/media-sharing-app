d2cb5084499db354fe0318dcf5f6dd812a1b2c6c
Dipu <dipuchaurasiya91@gmail.com>
2023-11-16 22:15:21 +0545

    css variable added

--- src/components/Message/SingleMessage.tsx
+++ src/components/Message/SingleMessage.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -10,35 +11,39 @@ import {
 import clsx from "clsx";
 
 const SingleMessage = () => {
+  React.useEffect(() => {
+    document.body.classList.add("light");
+  }, []);
+
   return (
-    <div className="  flex py-2 px-3 hover:bg-neutral-600">
-      <Avatar className="w-10 h-10">
+    <div className="  flex px-3 py-2 hover:bg-neutral-600">
+      <Avatar className="h-10 w-10">
         <AvatarImage src="https://lh3.googleusercontent.com/a/AAcHTtemBCO2OhcBqzCqMAYD8lPrguhdxVd4Ff4_vE7mKn_BEEs=s1000-c" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
       <div className="ml-2 w-full">
         <div className="flex items-center gap-3">
-          <h4 className="text-neutral-100 text-sm font-medium w-1/2 overflow-hidden whitespace-nowrap overflow-ellipsis   ">
+          <h4 className="light:text-green-500 w-1/2 overflow-hidden overflow-ellipsis whitespace-nowrap text-sm font-medium text-neutral-100  dark:text-blue-700  ">
             Dipu chaurasiya
           </h4>
         </div>
 
         <div className=" relative ">
-          <p className="text-neutral-200 line-clamp-3  w-full block  font-light pb-2  break-words text-xs mr-3   ">
+          <p className="mr-3 line-clamp-3  block w-full  break-words pb-2  text-xs font-light text-neutral-200   ">
             this is a t djf pdif oduf o udof oudof do u ofdijf oodus io sudfai
             odufoa efa oueo te
             <span className="">
               <span className="px-3">
                 {" "}
-                <span className="opacity-0 text-xs">
+                <span className="text-xs opacity-0">
                   {" "}
                   {new Date().toLocaleDateString()}
                 </span>{" "}
               </span>
             </span>
           </p>
-          <div className=" w-full flex justify-end absolute  right-5 bottom-1 ">
-            <span className="text-[0.65rem] text-neutral-200 font-extralight ">
+          <div className=" absolute bottom-1 right-5 flex  w-full justify-end ">
+            <span className="text-[0.65rem] font-extralight text-neutral-200 ">
               {new Date().toLocaleTimeString()}
             </span>
           </div>
--- src/components/room/rightsideBar/users/index.tsx
+++ src/components/room/rightsideBar/users/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <h3 className="light:text-neutral-900  text-neutral-300 dark:text-neutral-50 ">
+        Members
+      </h3>
+      <section></section>
+    </div>
+  );
 };
 
 export default index;
