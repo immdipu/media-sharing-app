045b43287b28bfabf34d7d2506e7ff28d27c4768
Dipu <dipuchaurasiya91@gmail.com>
2023-12-29 20:25:27 +0545

    Add MainMenu and activity types

--- src/components/Excalidraw/OtherUserExcalidraw.tsx
+++ src/components/Excalidraw/OtherUserExcalidraw.tsx
@@ -2,8 +2,11 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import { useSocket } from "@/context/SocketProvider";
 import { useToast } from "../ui/use-toast";
+import { MainMenu } from "@excalidraw/excalidraw";
 import { RoomContext } from "../room/SingleRoom/JoinedSingleRoom";
+import { IAddActivity, IRemoveActivity } from "@/types/socketTypes";
 import { useAppSelector } from "@/hooks/reduxHooks";
+import { ActivityType, IGetActivityTypes } from "@/types/roomActivity";
 
 const OtherUserExcalidraw = () => {
   const {
