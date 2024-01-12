b4b42cba2694b8b5a01a9ed34fd00aab7af46815
Dipu <dipuchaurasiya91@gmail.com>
2024-01-12 22:59:13 +0545

    Add Message component and AllMessages component

---
+++ src/components/Message/ChatsMessage/AllMessages.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllMessages = () => {
+  return <div>This is message</div>;
+};
+
+export default AllMessages;
---
+++ src/components/Message/ChatsMessage/Message.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="h-[calc(100vh-64px)] w-full bg-Main-background">
+      Message
+    </div>
+  );
+};
+
+export default Message;
