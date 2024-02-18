abd238b8598915b3ca30170d10bfc5816b850631
Dipu <dipuchaurasiya91@gmail.com>
2024-02-18 10:18:24 +0545

    refactor

--- src/components/card/RoomUpdateCard.tsx
+++ src/components/card/RoomUpdateCard.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { TbLogout2 } from "react-icons/tb";
-import { RoomJoinLeaveTypes } from "@/types/room";
-import { FaBell } from "react-icons/fa";
 import clsx from "clsx";
 
 const RoomUpdateCard = ({
@@ -13,7 +9,7 @@ const RoomUpdateCard = ({
   updatedBy: string;
 }) => {
   return (
-    <div className=" mx-3 my-1 flex min-h-[3rem] flex-col  rounded-md bg-third-background px-3 py-2  hover:bg-secondary-hover">
+    <div className=" bg-third-background mx-3 my-1 flex min-h-[3rem]  flex-col rounded-md px-3 py-2  hover:bg-secondary-hover">
       {/* <div className="ml-3">
         <FaBell className="text-2xl text-green-200" />
       </div> */}
