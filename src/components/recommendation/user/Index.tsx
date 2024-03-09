367fcad5f4172b93f4343c1473d06af8f95773db
Dipu <dipuchaurasiya91@gmail.com>
2024-03-09 20:29:38 +0545

    recommedation user completed

--- src/components/recommendation/user/Index.tsx
+++ src/components/recommendation/user/Index.tsx
@@ -1,10 +1,13 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { userApis } from "@/Apis/APIs";
 import RecommendationCard from "./organism/RecommendationCard";
+import { X } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const RecommedationUser = () => {
+  const [show, setShow] = useState(true);
   const { data, isLoading } = useQuery(["user"], () =>
     userApis.UserRecommendation(),
   );
@@ -13,24 +16,43 @@ const RecommedationUser = () => {
     return;
   }
 
-  if (!data) {
+  if (!data.data || data?.data?.length === 0) {
     return;
   }
 
   return (
-    <div>
-      {data.data &&
-        data.data.length > 0 &&
-        data.data.map((user: any) => (
-          <RecommendationCard
-            key={user._id}
-            profilPic={user.profilePic}
-            fullName={user.fullName}
-            username={user.username}
-            _id={user._id}
-          />
-        ))}
-    </div>
+    <>
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            initial={{ opacity: 1 }}
+            exit={{ opacity: 0, y: -200, transition: { duration: 0.5 } }}
+            className="rounded-md  bg-Secondary-background px-4 py-2"
+          >
+            <div className="flex justify-between">
+              <h3 className="mb-3 font-bold text-Header-secondary">
+                Who to follow
+              </h3>
+              <X
+                onClick={() => setShow(false)}
+                className=" h-4 w-4 cursor-pointer  text-paragraph-secondary duration-300 active:scale-125"
+              />
+            </div>
+            {data.data &&
+              data.data.length > 0 &&
+              data.data.map((user: any) => (
+                <RecommendationCard
+                  key={user._id}
+                  profilPic={user.profilePic}
+                  fullName={user.fullName}
+                  username={user.username}
+                  _id={user._id}
+                />
+              ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
